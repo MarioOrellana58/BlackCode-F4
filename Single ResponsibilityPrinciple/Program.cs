51020777c3d6b38b652af56d265bbe1e5f6cda23
jetejeda <eduardotejedaleon@gmail.com>
2021-07-12 09:13:19 -0600

    Before single responsibility principle example

---
+++ Single ResponsibilityPrinciple/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Single_ResponsibilityPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeBefore = new EmployeeBefore("Juan", "Project Manager", 26, 15000);
+            Console.WriteLine(employeeBefore);
+            employeeBefore.PayTaxes();
+            Console.ReadKey();
+        }
+    }
+}
