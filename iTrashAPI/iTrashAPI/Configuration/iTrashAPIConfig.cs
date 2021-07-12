f555a47e9768ad2309484e4feaee97cfdf7e849c
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2021-07-11 18:36:22 -0600

    Api finished

---
+++ iTrashAPI/iTrashAPI/Configuration/iTrashAPIConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace iTrashAPI.Configuration
+{
+    public class iTrashAPIConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ iTrashAPI/iTrashAPI/Controllers/iTrashController.cs
@@ -0,0 +1,25 @@
+﻿using iTrashAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace iTrashAPI.Controllers
+{
+    public class iTrashController : ApiController
+    {
+        iTrashDB _context;
+        public iTrashController()
+        {
+            _context = new iTrashDB();
+        }
+        [Route("api/iTrash")]
+        public string Get()
+        { 
+            var users = _context.Users.ToList();
+            return "Hello World";
+        }
+    }
+}
